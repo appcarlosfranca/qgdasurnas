5b03ca882f4e63204d3cfccf9383cc70fcdcee45
appcarlosfranca <carlosfrancamed@gmail.com>
2026-09-09 18:17:01 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* QG DAS URNAS v228 — Service Worker para GitHub Pages */
-const CACHE = 'qg-das-urnas-v228-static-1';
+/* QG DAS URNAS v229 — Service Worker para GitHub Pages */
+const CACHE = 'qg-das-urnas-v229-static-1';
 const CORE = [
   './',
   './index.html',
